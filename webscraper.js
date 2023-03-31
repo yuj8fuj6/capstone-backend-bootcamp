f8ed9ae6a974d8ada9d404da2fc741725bf853ca
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-03-31 11:32:31 +0800

    Added express methods and routes for scraping

--- webscraper.js
+++ webscraper.js
@@ -14,61 +14,67 @@ const scdfURL =
 const bcaURL =
   "https://www1.bca.gov.sg/about-us/news-and-publications/circulars";
 
-axios(uraURL)
-  .then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const uraCircular = [];
+app.get("/circulars/ura", (req, res) => {
+  axios(uraURL)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const uraCircular = [];
 
-    $(".horizontal-box", html).each(function () {
-      const date = $(this)
-        .find(".col-md-3.col-sm-3.col-xs-12.no-lr-padding")
-        .find(".text")
-        .text()
-        .trim();
-      const title = $(this)
-        .find(".col-md-9.col-sm-9.col-xs-12.no-lr-padding")
-        .find(".text")
-        .text()
-        .trim();
-      const url = "ura.gov.sg" + $(this).find("a").attr("href");
-      uraCircular.push({ date, title, url });
-    });
-    console.log(uraCircular);
-  })
-  .catch((err) => console.log(err));
+      $(".horizontal-box", html).each(function () {
+        const date = $(this)
+          .find(".col-md-3.col-sm-3.col-xs-12.no-lr-padding")
+          .find(".text")
+          .text()
+          .trim();
+        const title = $(this)
+          .find(".col-md-9.col-sm-9.col-xs-12.no-lr-padding")
+          .find(".text")
+          .text()
+          .trim();
+        const url = "ura.gov.sg" + $(this).find("a").attr("href");
+        uraCircular.push({ date, title, url });
+      });
+      res.json(uraCircular);
+    })
+    .catch((err) => console.log(err));
+});
 
-axios(scdfURL)
-  .then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const scdfCircular = [];
+app.get("/circulars/scdf", (req, res) => {
+  axios(scdfURL)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const scdfCircular = [];
 
-    $(".row.borderBotSilver", html).each(function () {
-      const date = $(this).find(".f-right").find(".d-in").text();
-      const title = $(this).find(".bold.color-333").text();
-      const url = $(this).find("a").attr("href");
-      scdfCircular.push({ date, title, url });
-    });
-    console.log(scdfCircular);
-  })
-  .catch((err) => console.log(err));
+      $(".row.borderBotSilver", html).each(function () {
+        const date = $(this).find(".f-right").find(".d-in").text();
+        const title = $(this).find(".bold.color-333").text();
+        const url = $(this).find("a").attr("href");
+        scdfCircular.push({ date, title, url });
+      });
+      res.json(scdfCircular);
+    })
+    .catch((err) => console.log(err));
+});
 
-axios(bcaURL)
-  .then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const bcaCircular = [];
+app.get("/circulars/bca", (req, res) => {
+  axios(bcaURL)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const bcaCircular = [];
 
-    $(".listItemWrap", html).each(function () {
-      const date = $(this).find(".sfnewsMetaInfo.sfmetainfo").text();
-      const title = $(this).find(".sfnewsTitle.sftitle").text().trim();
-      const url = $(this).find("a").attr("href");
-      bcaCircular.push({ date, title, url });
-    });
-    console.log(bcaCircular);
-  })
-  .catch((err) => console.log(err));
+      $(".listItemWrap", html).each(function () {
+        const date = $(this).find(".sfnewsMetaInfo.sfmetainfo").text();
+        const title = $(this).find(".sfnewsTitle.sftitle").text().trim();
+        const url = $(this).find("a").attr("href");
+        bcaCircular.push({ date, title, url });
+      });
+      res.json(bcaCircular);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
