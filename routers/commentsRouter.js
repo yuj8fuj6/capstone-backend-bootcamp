1bc259db2559743ce439ce2e20c3122a7458e113
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-04-07 11:14:22 +0800

    Resolved delete thread bug

--- routers/commentsRouter.js
+++ routers/commentsRouter.js
@@ -9,7 +9,7 @@ router.get("/:postID/allThreads", controllers.getAllThreads);
 router.post("/addPost", controllers.addOnePost);
 router.post("/addThread", controllers.addOneThread);
 router.delete("/deletePost", controllers.deleteOnePost);
-router.delete("/deleteThread", controllers.deleteOnePost);
+router.delete("/deleteThread", controllers.deleteOneThread);
 router.post("/addPostVote", controllers.addOnePostVote);
 router.post("/addOneThreadVote", controllers.addOneThreadVote);
 router.get("/:postID/count", controllers.getThreadCount);
