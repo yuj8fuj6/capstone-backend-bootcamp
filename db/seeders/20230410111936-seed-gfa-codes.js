50b03980179f848cc60f98e89b44b14a41a5bae6
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-04-24 01:32:24 +0900

    Seeded building code data

--- db/seeders/20230410111936-seed-gfa-codes.js
+++ db/seeders/20230410111936-seed-gfa-codes.js
@@ -227,191 +227,191 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date(),
         },
-        {
-          authority_id: 1,
-          header: "Mechanical and Electrical (M&E) Spaces",
-          content:
-            "For M&E floors with varying headroom (i.e. the height of the headroom varies from below 1.8m to exceeding 1.8m), the entire floor is included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MechanicalandElectricalMESpaces",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Mechanical and Electrical (M&E) Spaces",
-          content:
-            "M&E rooms (including bin centres and substations) within fully sunken common basement carparks are excluded from GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MechanicalandElectricalMESpaces",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Metal Ceiling Grids within Indoor Performance Venue",
-          content:
-            "Ceiling grids located directly above a stage area within an indoor performance venue are excluded from GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MetalCeilingGridswithinIndoorPerformanceVenue",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Meter Compartments",
-          content:
-            "Meter compartments incorporated inside gateposts are excluded from GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MeterCompartments",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Planter Boxes",
-          content:
-            "Communal planter boxes are excluded from GFA if they comply with certain requirements.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/PlanterBoxes",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Planter Boxes",
-          content:
-            "Private planter boxes located within strata units of both residential and non-residential developments are included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/PlanterBoxes",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Racking Systems for Storage Purposes",
-          content:
-            "Racking systems are excluded from GFA if they are standalone modular racking systems that do not form an integral component of the building (e.g. not of reinforced concrete construction), and are used for storage purposes in industrial developments.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RackingSystemsforStoragePurposes",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Recessed Windows",
-          content:
-            "Area under a recessed window, regardless of height of the window, is included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RecessedWindows",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Refuse Chambers and Refuse Chutes",
-          content: "Refuse chutes are excluded from GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RefuseChambersandRefuseChutes",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Refuse Chambers and Refuse Chutes",
-          content:
-            "Refuse chambers and Discharge Valve (DV) chambers (part of a pneumatic system) located within fully sunken basement carparks are excluded from GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RefuseChambersandRefuseChutes",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Reinforced Concrete (RC) Slabs within Voids",
-          content:
-            "Covered RC slabs within voids (including service riser voids and voids beside storey shelter) are included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/ReinforcedConcreteRCSlabswithinVoids",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Service Ducts",
-          content:
-            "Service ducts, including the thickness of the walls, are included as GFA once at the lowest floor, if the vertical shaft is entirely a void space.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/ServiceDucts",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Solar Panels",
-          content:
-            "Area shadowed by solar panel is excluded from GFA unless the shadowed area is enclosed, used for commercial uses (e.g. outdoor refreshment area) or storage purpose.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/SolarPanels",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Staircases",
-          content:
-            "Staircases are included as GFA. The flight of the staircase is to be projected upwards and be included as GFA on that particular floor.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Staircases",
-          content:
-            "Staircases at intermediate levels not connected to any floor are not included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Staircases",
-          content:
-            "All uncovered external perforated staircases, regardless of materials, are included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Staircases",
-          content:
-            "For pure industrial/warehouse developments, uncovered external mild steel perforated staircases are excluded from GFA provided the development already provides a covered internal staircase to serve users of the building. If the width of the uncovered external mild steel perforated staircase (including the staircase landing) is more than 1.0m, the developer shall provide an undertaking not to cover up the external staircase subsequently.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Staircases",
-          content: "Uncovered staircase to ESS are excluded from GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Swimming Pool",
-          content: "Covered swimming pool is included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/SwimmingPool",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Walls and Columns",
-          content: "Walls and columns are included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/WallsandColumns",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          authority_id: 1,
-          header: "Water Tanks",
-          content:
-            "Water tanks, including sprinkler tanks, balancing tanks and storm water detention tanks, are excluded from GFA. Rooms that house water tanks are included as GFA.",
-          url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/WaterTanks",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
+        // {
+        //   authority_id: 1,
+        //   header: "Mechanical and Electrical (M&E) Spaces",
+        //   content:
+        //     "For M&E floors with varying headroom (i.e. the height of the headroom varies from below 1.8m to exceeding 1.8m), the entire floor is included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MechanicalandElectricalMESpaces",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Mechanical and Electrical (M&E) Spaces",
+        //   content:
+        //     "M&E rooms (including bin centres and substations) within fully sunken common basement carparks are excluded from GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MechanicalandElectricalMESpaces",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Metal Ceiling Grids within Indoor Performance Venue",
+        //   content:
+        //     "Ceiling grids located directly above a stage area within an indoor performance venue are excluded from GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MetalCeilingGridswithinIndoorPerformanceVenue",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Meter Compartments",
+        //   content:
+        //     "Meter compartments incorporated inside gateposts are excluded from GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/MeterCompartments",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Planter Boxes",
+        //   content:
+        //     "Communal planter boxes are excluded from GFA if they comply with certain requirements.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/PlanterBoxes",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Planter Boxes",
+        //   content:
+        //     "Private planter boxes located within strata units of both residential and non-residential developments are included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/PlanterBoxes",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Racking Systems for Storage Purposes",
+        //   content:
+        //     "Racking systems are excluded from GFA if they are standalone modular racking systems that do not form an integral component of the building (e.g. not of reinforced concrete construction), and are used for storage purposes in industrial developments.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RackingSystemsforStoragePurposes",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Recessed Windows",
+        //   content:
+        //     "Area under a recessed window, regardless of height of the window, is included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RecessedWindows",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Refuse Chambers and Refuse Chutes",
+        //   content: "Refuse chutes are excluded from GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RefuseChambersandRefuseChutes",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Refuse Chambers and Refuse Chutes",
+        //   content:
+        //     "Refuse chambers and Discharge Valve (DV) chambers (part of a pneumatic system) located within fully sunken basement carparks are excluded from GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/RefuseChambersandRefuseChutes",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Reinforced Concrete (RC) Slabs within Voids",
+        //   content:
+        //     "Covered RC slabs within voids (including service riser voids and voids beside storey shelter) are included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/ReinforcedConcreteRCSlabswithinVoids",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Service Ducts",
+        //   content:
+        //     "Service ducts, including the thickness of the walls, are included as GFA once at the lowest floor, if the vertical shaft is entirely a void space.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/ServiceDucts",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Solar Panels",
+        //   content:
+        //     "Area shadowed by solar panel is excluded from GFA unless the shadowed area is enclosed, used for commercial uses (e.g. outdoor refreshment area) or storage purpose.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/SolarPanels",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Staircases",
+        //   content:
+        //     "Staircases are included as GFA. The flight of the staircase is to be projected upwards and be included as GFA on that particular floor.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Staircases",
+        //   content:
+        //     "Staircases at intermediate levels not connected to any floor are not included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Staircases",
+        //   content:
+        //     "All uncovered external perforated staircases, regardless of materials, are included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Staircases",
+        //   content:
+        //     "For pure industrial/warehouse developments, uncovered external mild steel perforated staircases are excluded from GFA provided the development already provides a covered internal staircase to serve users of the building. If the width of the uncovered external mild steel perforated staircase (including the staircase landing) is more than 1.0m, the developer shall provide an undertaking not to cover up the external staircase subsequently.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Staircases",
+        //   content: "Uncovered staircase to ESS are excluded from GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/Staircases",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Swimming Pool",
+        //   content: "Covered swimming pool is included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/SwimmingPool",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Walls and Columns",
+        //   content: "Walls and columns are included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/WallsandColumns",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
+        // {
+        //   authority_id: 1,
+        //   header: "Water Tanks",
+        //   content:
+        //     "Water tanks, including sprinkler tanks, balancing tanks and storm water detention tanks, are excluded from GFA. Rooms that house water tanks are included as GFA.",
+        //   url: "www.ura.gov.sg/Corporate/Guidelines/Development-Control/gross-floor-area/GFA/WaterTanks",
+        //   created_at: new Date(),
+        //   updated_at: new Date(),
+        // },
       ],
       {},
     );
